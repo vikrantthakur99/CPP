ad0ebff99e2e5af171cd6b1da50649b8114dfb59
vikrantthakur99 <86282855+vikrantthakur99@users.noreply.github.com>
2021-07-02 23:20:21 +0530

    Add files via upload

---
+++ whileloop.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main ()
+{
+int n,i=1;     //set value of n to which value you want to print your number
+
+cout<<"Enter the value of n:";
+cin>>n;
+while (i<=n)
+{
+cout<<i<<endl;
+i++;
+}
+  return 0;
+}
