58fadcff2c80ad22cc2bdd9993ab49825fa77b48
vikrantthakur99 <86282855+vikrantthakur99@users.noreply.github.com>
2021-07-03 16:02:12 +0530

    Add files via upload

---
+++ tableprogram_by_forloop.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main ()
+{
+   int n,i;
+   cout<<"Enter the value of n :";
+   cin>>n;
+   for(i=1;i<=10;i++)
+   {
+    cout<<n<<" X "<<i<<" = "<<i*n<<endl; a
+   }
+   return 0;
+}
