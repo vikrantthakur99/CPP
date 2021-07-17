02419a075ce97d0b9e151035d8b3a97405068540
Vikrant Thakur <86282855+vikrantthakur99@users.noreply.github.com>
2021-07-17 17:06:47 +0530

    Add files via upload

---
+++ arraybyforloop.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int i;    //initilization i
+  int A[5]={4,6,7,8,9};     
+    for(i=0;i<5;i++)    //for loop is used to print or access the elements in the array
+    { 
+    	cout<<A[i]<<endl;
+	}  
+    return 0;
+}
