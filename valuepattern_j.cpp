991b37d728936c45a47d3b73ba02ebdcdb1c2538
Vikrant Thakur <86282855+vikrantthakur99@users.noreply.github.com>
2021-07-22 23:08:25 +0530

    Add files via upload

---
+++ valuepattern_j.cpp
@@ -0,0 +1,16 @@
+ #include <iostream>
+using namespace std;
+
+int main()
+{
+	int i,j;
+    for(i=1;i<=4;i++)        
+    {
+    	for(j=1;j<=4;j++)    //this loop will print the rows simill..
+    	{
+    		cout<<" "<<j;      //this will print only the value for j that is 1 2 3 4
+		}
+		cout<<endl;       //it will end the line for the rows
+	}
+    return 0;
+}
