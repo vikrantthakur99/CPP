500a989421d5cd52351217dbc0a1ab5ae492f493
Vikrant Thakur <86282855+vikrantthakur99@users.noreply.github.com>
2021-07-23 01:16:02 +0530

    Add files via upload

---
+++ matricesmulti_array.cpp
@@ -0,0 +1,26 @@
+ #include <iostream>
+using namespace std;
+
+int main()
+{
+	int A[2][3]={{2,5,9},{6,9,15}};
+	int B[2][3]={{1,3,4},{1,1,1}};
+	int C[2][3];
+	int i,j;
+    for(i=0;i<2;i++)        
+    {
+    	for(j=0;j<3;j++) 
+    	{
+    	C[i][j]=A[i][j]*B[i][j];      
+		}
+	}
+	for(i=0;i<2;i++)
+	{
+		for(j=0;j<3;j++)
+		{
+			cout<<" "<<C[i][j];
+		}
+	}
+	cout<<endl;
+    return 0;  
+}
