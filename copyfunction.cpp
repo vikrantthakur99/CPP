66ab9addec0e61ee26ca46cb28ce242cef78d397
Vikrant Thakur <86282855+vikrantthakur99@users.noreply.github.com>
2021-07-24 23:49:02 +0530

    Add files via upload

---
+++ copyfunction.cpp
@@ -0,0 +1,16 @@
+ #include <iostream>
+ #include <cstring>
+using namespace std;
+
+int main()
+{    
+    
+     // **3. strncpy(): COPY THE SOURCE TO DESTINATION OF TWO STRINGS 
+    char s1[20]= "Good";
+    char s2[20]= "";
+    
+    strncpy(s2,s1,2);
+    cout<<s2; */
+    
+    return 0;   
+}
