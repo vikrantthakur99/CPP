1af79a9bb5d2c79c7856120edf108950b8ecf1ff
Vikrant Thakur <86282855+vikrantthakur99@users.noreply.github.com>
2021-07-24 23:49:17 +0530

    Add files via upload

---
+++ concatfunction.cpp
@@ -0,0 +1,17 @@
+ #include <iostream>
+ #include <cstring>
+using namespace std;
+
+int main()
+{    
+ 
+	 // **2. strncat(): CONCATENATE TWO STRINGS 
+    char s1[20]="Hello";
+    char s2[30]="World";
+    
+    strncat(s1,s2,3);  //we can also use strcat but it doesnot allow us to choose the number from the second string to concat. we can use this by using strncat
+    cout<<s1<<endl; */
+
+    
+    return 0;   
+}
