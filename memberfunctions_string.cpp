f90ba2f34978f2fc41b101167dec5ca532834293
Vikrant Thakur <86282855+vikrantthakur99@users.noreply.github.com>
2021-07-25 22:35:11 +0530

    Add files via upload

---
+++ memberfunctions_string.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+	/*
+	// 1. s.append()- IT APPENDS THE GIVEN STRINGS
+	string str = "Hello ";
+	str.append("World"); 
+	cout<<str;
+
+	//2. s.insert(3,"") - IT INSERTS THE ELEMENT AT THE DESRIRED LOC
+	string str = "programming";
+	str.insert(2,"KK");
+	cout<<str;
+	
+	
+	//3. s.replace(3,4,"") - IT REPLCAES THE ELEMENT AT THE DESRIRED LOC BY DEFINING HOW MANY WORDS THAT YOU WANT TO REPLACE
+	string str = "helloworld";
+	str.replace(2,3,"OO");
+	cout<<str;
+	
+	//4. s.erase() - IT ERASES THE STRING
+	string str = "helloworld";
+	str.erase();
+	cout<<str;
+    
+	
+	//5. s.push_back() - IT PUSHES THE ELEMENT AT THE END
+	string str = "helloworld";
+	str.push_back('A');
+	cout<<str;
+	
+	//6. s.pop_back() - IT POP THE ELEMENT AT THE END 
+	
+    //7. s1.swap(s2) - IT WILL SWAP THE BOTH STRINGS
+    string str1 = "Hello";
+    string str2 = "World";
+    str1.swap(str2);
+    cout<<str1<<" "<<str2<<endl;  //displaying the both swap strings
+    */
+	
+    return 0;   
+}
