2d40e1376d3e7a891a261417107750c861cf6c46
Vikrant Thakur <86282855+vikrantthakur99@users.noreply.github.com>
2021-07-26 23:42:45 +0530

    Add files via upload

---
+++ operators_string.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+    /*
+	string str1 = "Hello";
+	string str2 = " World";
+	str1 = str1+str2;     //+ operator is used to concat.. the  2 strings
+	cout<<str1; */
+	
+	string str1 = "Hello";
+	string str2 = " World";
+ str1=str2;           //= operator is used to assigns.. the  other string
+	cout<<str1;
+    cout<<str2;
+	
+    return 0;   
+}
