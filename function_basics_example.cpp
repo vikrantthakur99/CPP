5d25860050960d4a7dc5269391670c727dca9d23
Vikrant Thakur <86282855+vikrantthakur99@users.noreply.github.com>
2021-07-28 21:37:12 +0530

    Add files via upload

---
+++ function_basics_example.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+using namespace std;
+
+    void message()   //declaration of a function i.e. void function_name()
+    {
+      cout<<"Hello";
+  }
+
+    int main()
+   { 
+  message();      //calling of the function inside main by calling function name
+    return 0;
+   }
+
+    //FUNCTION BASICS WITH EXAMPLES 
+    /* FUNCTION PROGRAM FOR ADDING FOR 2 NUMBERS
+
+  float add(float x, float y)  
+{
+  float z;
+    z=x+y;
+    return z;
+}
+  int main()
+{
+  float num1,num2,num3;
+  cout<<"Enter your First number ";
+  cin>>num1;
+  cout<<"Enter your Second number ";
+  cin>>num2;
+  
+  num3=add (num1,num2);
+  cout<<"The result is "<<num3<<endl; 
+  
+    return 0;
+}
+*/
